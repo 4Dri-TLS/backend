42eeb4ea702c8eef9087523a4bcffe0b0b75f9f3
Adrien Ruffié <ruffieadrien@gmail.com>
2025-01-02 18:23:19 +0100

    Premier commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+const mongoose = require('mongoose'); //Import du package mongoose
+
+app.use(express.json()); //sert à transformer le corps de la requête en objet JS utilisable
+
+mongoose.connect(process.env.BDD_URL,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//Middleware qui ajoute des headers à l'objet réponse pour éviter les erreurs de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+});
+
+//Middleware qui répond aux requêtes GET envoyées à /api/stuff
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+
+module.exports = app;
